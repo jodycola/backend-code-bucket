c00ac3e02c02f2fc5f093a7e75012237fd7ae847
Kody Samaroo <kody.samaroo@gmail.com>
2021-09-20 23:36:30 -0400

    building user login/signup/auth routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+
+    def auth
+        render json: @current_user
+    end
+
+    def login
+        user = User.find_by(name: params[:name])
+        if user && user.authenticate(params[:password])
+            token = JWT.encode({ user_id: user.id }, '$3cr3t', 'HS256')
+            render json: { user: UserSerializer.new(user), token: token }
+        else
+            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+        end
+    end
+
+    def signup
+        user = User.create(name: params[:name], password: params[:password])
+        if user.valid?
+            user && user.authenticate(params[:password])
+            token = JWT.encode({ user_id: user.id }, '$3cr3t', 'HS256')
+            render json: { user: UserSerializer.new(user), token: token }
+        else
+            render json: { errors: users.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
     def index
         @users = User.all
         render json: @users
@@ -7,11 +33,6 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         render json: @user
-    end
-
-    def get_all_names
-        @users = User.pluck(:name)
-        render json: @users
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,4 @@ class User < ApplicationRecord
     has_many :collections
     has_many :projects, through: :collections
 
-    def make_json_object
-        {
-            name: self.name
-            # collections: self.collections
-        }
-    end
-
 end
